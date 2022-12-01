82ea8ff2684bff4879b07769a8dea11adaccfb50
Gauri Patil <gauri.be0811@gmail.com>
2022-12-02 01:10:54 +0530

    Added some meaningful messages when invalid inputs are passed

--- SecondsMinutesTimeOverloading/src/Main.java
+++ SecondsMinutesTimeOverloading/src/Main.java
@@ -3,9 +3,18 @@ public class Main {
         getDurationString(8274);
         getDurationString(7356473);
         getDurationString(0);
+        getDurationString(-34);
+        getDurationString(10, 23);
+        getDurationString(76, 23);
+        getDurationString(13, -34);
+        getDurationString(13, 3443);
     }
 
     public static void getDurationString(int seconds) {
+        if (seconds < 0) {
+            System.out.println("\nInvalid seconds input value - " +  seconds);
+            return;
+        }
         int minutes = seconds / 60;
         int remainingSeconds = seconds % 60;
         getDurationString(remainingSeconds, minutes);
@@ -13,6 +22,14 @@ public class Main {
     }
 
     public static void getDurationString(int seconds, int minutes) {
+        if (seconds < 0 || seconds > 59) {
+            System.out.println("\nInvalid seconds input value - " + seconds);
+            return;
+        }
+        if (minutes < 0) {
+            System.out.println("\nInvalid minutes input value - " + minutes);
+            return;
+        }
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
         System.out.printf("\n%dh %dm %ds", hours, remainingMinutes, seconds);
