ef41e8d5a31d10e7f05c3f64edd80152cc588cda
Gauri Patil <gauri.be0811@gmail.com>
2022-12-31 20:25:54 +0530

    Arrays var args & ... operator

---
+++ Arrays/ArrayIntro/src/ArrayVarargs.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class ArrayVarargs {
+
+    public static void main(String... args) {
+
+        System.out.println("Hello World again");
+
+        String[] splitStrings = "Hello World again".split(" ");
+        printText(splitStrings);
+
+        System.out.println("_".repeat(20));
+        printText("Hello");
+
+        System.out.println("_".repeat(20));
+        printText("Hello", "World", "again", "!!");
+
+        System.out.println("_".repeat(20));
+        printText();
+
+        String[] sArray = {"first", "second", "third", "fourth", "fifth"};
+        System.out.println(String.join(",", sArray));
+    }
+
+    private static void printText(String... textList) {
+        System.out.println(Arrays.toString(textList));
+
+        for (String t : textList) {
+            System.out.println(t);
+        }
+    }
+}
+
