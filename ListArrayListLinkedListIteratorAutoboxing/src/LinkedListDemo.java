8b0696aff43ba437c23f0f45f7ea24b35f39baac
Gauri Patil <gauri.be0811@gmail.com>
2023-01-07 02:29:15 +0530

    LinkedList Demo app in progress

--- ListArrayListLinkedListIteratorAutoboxing/src/LinkedListDemo.java
+++ ListArrayListLinkedListIteratorAutoboxing/src/LinkedListDemo.java
@@ -12,6 +12,74 @@ public class LinkedListDemo {
         // Add elements
         placesToVisit.add("Pune");
         placesToVisit.add(0,"Mumbai");
+
+        addMoreElements(placesToVisit);
         System.out.println(placesToVisit);
+
+        removeMoreElements(placesToVisit);
+        System.out.println(placesToVisit);
+    }
+
+    public static void addMoreElements(LinkedList<String> placesToVisit){
+        //LIST METHODS
+        placesToVisit.addFirst("Ahmednagar");
+        placesToVisit.addLast("Dhule");
+
+
+        // QUEUE METHODS
+        // Adds element to tail (last) elements of the list
+        placesToVisit.offer("Nashik");
+        // Adds element at front of the list
+        placesToVisit.offerFirst("Solapur");
+        // Insert at the end of list
+        placesToVisit.offerLast("Kolhapur");
+
+
+        // STACK METHODS
+        // Pushes the element onto stack represented by this list ie at the front of the list
+        placesToVisit.push("Nanded");
+    }
+
+    public static void removeMoreElements(LinkedList<String> placesToVisit) {
+        placesToVisit.remove(1);
+        placesToVisit.remove("Nashik");
+        System.out.println(placesToVisit);
+
+        // LIST METHODS
+        // removes the head of the list
+        String s1 = placesToVisit.remove();
+        System.out.println("remove() - " + s1 + " was removed");
+
+        // removes the head of the list
+        String s2 = placesToVisit.removeFirst();
+        System.out.println("removeFirst() - " + s2 + " was removed");
+
+        // removes the tail of the list
+        String s3 = placesToVisit.removeLast();
+        System.out.println("removeLast() - " + s3 + " was removed");
+
+        //QUEUE/DEQUEUE POLL METHODS
+        // removes the head of the list
+        String p1 = placesToVisit.poll();
+        System.out.println("poll() - " + p1 + " was removed");
+
+        // removes the head of the list
+        String p2 = placesToVisit.pollFirst();
+        System.out.println("pollFirst() - " + p2 + " was removed");
+
+        // removes the tail of the list
+        String p3 = placesToVisit.pollLast();
+        System.out.println("pollLast() - " + p3 + " was removed");
+
+
+        // STACK PUSH & POP METHODS
+        placesToVisit.push("Kalyan");
+        placesToVisit.push("Bandra");
+        placesToVisit.push("Ville Parle");
+        System.out.println(placesToVisit);
+
+        // removes & return the first element of ghe list
+        String p4 = placesToVisit.pop();
+        System.out.println(p4 + " was removed");
     }
 }
