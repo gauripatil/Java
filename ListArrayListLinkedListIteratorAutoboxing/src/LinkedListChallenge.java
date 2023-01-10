2167e863221c4ca04fee1a02073e722308e7ad41
Gauri Patil <gauri.be0811@gmail.com>
2023-01-10 20:20:57 +0530

    LinkedList challenge app - Places to visit app - forwad & backward traversal & adjusting the cursor position based on direction

--- ListArrayListLinkedListIteratorAutoboxing/src/LinkedListChallenge.java
+++ ListArrayListLinkedListIteratorAutoboxing/src/LinkedListChallenge.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String name, int distance) {
     @Override
@@ -18,6 +19,72 @@ public class LinkedListChallenge {
         addPlaces(placesToVisit, new Place("Banglore", 840));
         addPlaces(placesToVisit, new Place("Pune", 0));
         System.out.println(placesToVisit);
+
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = false;
+
+        printMenu();
+        while (!quitLoop) {
+
+            if(!iterator.hasPrevious()){
+                System.out.println("Originating: "+ iterator.next());
+                forward = true;
+            }
+
+            if(!iterator.hasNext()){
+                System.out.println("Final: " + iterator.previous());
+                forward = false;
+            }
+
+            System.out.print("Enter value:");
+            String menuItem = scanner.nextLine().toUpperCase().substring(0, 1);
+
+
+            switch (menuItem) {
+                case "F" :
+                    System.out.println("User wants to go forward:");
+                    // Reversing Direction
+                    if(!forward) {
+                        forward = true;
+                        // Adjusting position
+                        if(iterator.hasNext()) {
+                            iterator.next();
+                        }
+                    }
+
+                    if(iterator.hasNext()){
+                        System.out.println(iterator.next());
+                    }
+                    break;
+                case "B" :
+                    System.out.println("User wants to go backwards:");
+                    // Reversing direction
+                    if(forward) {
+                        forward = false;
+                        // Adjusting position
+                        if(iterator.hasPrevious()) {
+                            iterator.previous();
+                        }
+                    }
+                    if(iterator.hasPrevious()){
+                        System.out.println(iterator.previous());
+                    }
+                    break;
+                case "M" :
+                    printMenu();
+                    break;
+                case "L" :
+                    System.out.println(placesToVisit);
+                    break;
+                default:
+                    quitLoop = true;
+                break;
+            }
+        }
+
 
     }
 
@@ -47,5 +114,16 @@ public class LinkedListChallenge {
             matchedIndex++;
         }
         placesToVisit.add(place);
+    }
+
+    public static void printMenu() {
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit
+                """);
     }
 }
