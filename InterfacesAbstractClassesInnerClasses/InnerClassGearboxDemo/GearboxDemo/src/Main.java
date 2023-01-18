5e0ebeb7f29658c00d1a48f81ceda7f454435bda
Gauri Patil <gauri.be0811@gmail.com>
2023-01-18 23:56:00 +0530

    Gearbox Inner class demo - wip 2

--- InterfacesAbstractClassesInnerClasses/InnerClassGearboxDemo/GearboxDemo/src/Main.java
+++ InterfacesAbstractClassesInnerClasses/InnerClassGearboxDemo/GearboxDemo/src/Main.java
@@ -3,10 +3,28 @@ public class Main {
 
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+//        mcLaren.addGear(1, 5.3);
+//        mcLaren.addGear(2, 10.6);
+//        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+
+
+        // Below commented code will give compile time error due to incorrect way
+//        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
 //        Gearbox.Gear second = new Gearbox.Gear(2, 15.4);
 //        Gearbox.Gear third = new mcLaren.Gear(3, 17.8);
-        System.out.println(first.driveSpeed(1000));
+//        System.out.println(first.driveSpeed(1000));
+
+
 
     }
 }
