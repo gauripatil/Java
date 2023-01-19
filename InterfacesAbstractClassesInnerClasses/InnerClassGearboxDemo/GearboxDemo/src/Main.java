ee237ef4e6b8b7d71682ba27c293f3078e468902
Gauri Patil <gauri.be0811@gmail.com>
2023-01-19 23:32:01 +0530

    Gearbox Inner class demo- anonymous & named inner class - wip 3

--- InterfacesAbstractClassesInnerClasses/InnerClassGearboxDemo/GearboxDemo/src/Main.java
+++ InterfacesAbstractClassesInnerClasses/InnerClassGearboxDemo/GearboxDemo/src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 
 public class Main {
-
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
 //        mcLaren.addGear(1, 5.3);
@@ -25,6 +27,42 @@ public class Main {
 //        System.out.println(first.driveSpeed(1000));
 
 
+//          NAMED CLASS
+//        class ClickListener implements Button.OnClickListener {
+//            public ClickListener() {
+//                System.out.println("I've been attached");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//
+//        btnPrint.setOnClickListener(new ClickListener());
+        // ANONYMOUS CLASS - THIS IS SAME AS ABOVE NAMED CLASS - BOTH WILL WORK IN THE SAM WAY
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
 
+    }
+    private static void listen() {
+        boolean quit = false;
+        while(!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+
+            }
+        }
     }
 }
