0d54458f510ed67e99e9b2f95c875b49892e9fee
Gauri Patil <gauri.be0811@gmail.com>
2023-01-22 21:51:46 +0530

    Abstract Class - Custom Linked List Implementation - part 3 done

--- InterfacesAbstractClassesInnerClasses/AbstractClassCustomListChallenge/src/Main.java
+++ InterfacesAbstractClassesInnerClasses/AbstractClassCustomListChallenge/src/Main.java
@@ -84,5 +84,43 @@ public class Main {
         list.traverse(list.getRoot());
         list.removeItem(list.getRoot());
         list.traverse(list.getRoot());
+
+//        ========TREE========
+        System.out.println("------TREE-----");
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
+        // Create a string data array to avoid typing loads of addItem instructions:
+        String stringData2 = "5 7 3 9 8 2 1 0 4 6";
+//        String stringData2 = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+
+
+        String[] data2 = stringData2.split(" ");
+        for (String s : data2) {
+            tree.addItem(new Node(s));
+        }
+
+
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("3"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("5"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("0"));
+        tree.removeItem(new Node("4"));
+        tree.removeItem(new Node("2"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("9"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("8"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("6"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
     }
 }
