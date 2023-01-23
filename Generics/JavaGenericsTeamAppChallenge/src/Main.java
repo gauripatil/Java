fabfaa83efa9ff5ab33392373c98551fad069a74
Gauri Patil <gauri.be0811@gmail.com>
2023-01-23 19:43:15 +0530

    Generics - Player App Challenge part 1

---
+++ Generics/JavaGenericsTeamAppChallenge/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BasketballPlayer pat = new BasketballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+    }
+}
