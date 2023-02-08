4972dea1f9afce87d61b5f187795f634e6a235d1
Gauri Patil <gauri.be0811@gmail.com>
2023-02-08 23:49:26 +0530

    Collection Map - Location Adventure Game App - part 4 - immutable classes

--- Collections/MapLocationsApp/src/Location.java
+++ Collections/MapLocationsApp/src/Location.java
@@ -2,23 +2,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Location {
-    private final int locationId;
+    private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationId, String description) {
-        this.locationId = locationId;
+    public Location(int locationID, String description, Map<String, Integer> exits) {
+        this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        this.exits = new HashMap<String, Integer>(exits);
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
-    public int getLocationId() {
-        return locationId;
+    public int getLocationID() {
+        return locationID;
     }
 
     public String getDescription() {
