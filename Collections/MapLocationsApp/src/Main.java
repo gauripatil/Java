9c6f994187257bf65e8d54328d3fad53e236988b
Gauri Patil <gauri.be0811@gmail.com>
2023-02-09 02:09:08 +0530

    Collection Map - Location Adventure Game App - part 4 - immutable classes - added null check

--- Collections/MapLocationsApp/src/Main.java
+++ Collections/MapLocationsApp/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 2);
