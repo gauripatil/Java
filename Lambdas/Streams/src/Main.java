fc31374de4aa8b44658e32aeccaeb0fe10f27972
Gauri Patil <gauri.be0811@gmail.com>
2023-03-13 23:23:16 +0530

    Lambda Expressions - Sterams - FlapMap - Collect - part 3

--- Lambdas/Streams/src/Main.java
+++ Lambdas/Streams/src/Main.java
@@ -61,5 +61,45 @@ public class Main {
                 .distinct()
                 .peek(System.out::println)
                 .count());
+
+
+        ///// ******************* FLAT MAP **************************
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        // departments is a list of hr & accounting department
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        // FlatMap is used when list is not  source but it`s content is a source.
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+
+        System.out.println("---------------");
+
+        //////////////// ****************  COLLECT ********************
+        List<String> sortedGNumbers = someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+        for(String s : sortedGNumbers) {
+            System.out.println(s);
+        }
     }
 }
