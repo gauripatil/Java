dd71c6c1873111964b2dd6d1556dbf41301cc8d5
Gauri Patil <gauri.be0811@gmail.com>
2023-03-26 21:53:07 +0530

    Regular Expression - part 6

--- RegulrExpression/RegexApp/src/Main.java
+++ RegulrExpression/RegexApp/src/Main.java
@@ -127,19 +127,84 @@ public class Main {
         // NOT
         // [^abc]
         String tvTest = "tstvtkt";
-//        String tNotVRegExp = "t[^v]";
-
-        // Negative look ahead
-        String tNotVRegExp = "t(?!v)";
+        // t must be followed by any character apart from v
+        String tNotVRegExp = "t[^v]";
         Pattern tNotVPattern = Pattern.compile(tNotVRegExp);
         Matcher tNotVMatcher = tNotVPattern.matcher(tvTest);
 
-        count = 0;
-        while(tNotVMatcher.find()) {
-            count++;
+        while (tNotVMatcher.find()){
+            count = 0;
             System.out.println("Occurrence " + count + " : " + tNotVMatcher.start() + " to " + tNotVMatcher.end());
         }
 
+        // OUTPUT:
+        // Occurrence 0 : 0 to 2
+        // Occurrence 0 : 4 to 6
+
+
+        System.out.println("------NEGATIVE LOOK AHEAD---------");
+
+        // Negative look ahead
+        String tNotVRegExp1 = "t(?!v)";
+        Pattern tNotVPattern1 = Pattern.compile(tNotVRegExp1);
+        Matcher tNotVMatcher1 = tNotVPattern1.matcher(tvTest);
+
+        count = 0;
+        while(tNotVMatcher1.find()) {
+            count++;
+            System.out.println("Occurrence " + count + " : " + tNotVMatcher1.start() + " to " + tNotVMatcher1.end());
+        }
+
+        // OUTPUT:
+        // Occurrence 1 : 0 to 1
+        // Occurrence 2 : 4 to 5
+        // Occurrence 3 : 6 to 7
+
+        System.out.println("------POSITIVE LOOK AHEAD---------");
+
+        // POSITIVE look ahead
+        String tNotVRegExp2 = "t(?=v)";
+        Pattern tNotVPattern2 = Pattern.compile(tNotVRegExp2);
+        Matcher tNotVMatcher2 = tNotVPattern2.matcher(tvTest);
+
+        count = 0;
+        while(tNotVMatcher2.find()) {
+            count++;
+            System.out.println("Occurrence " + count + " : " + tNotVMatcher2.start() + " to " + tNotVMatcher2.end());
+        }
+
+        // OUTPUT:
+        // Occurrence 1 : 2 to 3
+
+
+        System.out.println("\n-------------- US PHONE NUMBER VALIDATION -------------");
+        // ^([\(]{1}[0-9]{3}[\)]{1}[ ]{1}[0-9]{3}[\-]{1}[0-9]{4})$
+        String usPhoneNumberRegex = "^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$";
+        String phone1 = "1234567890";  // Shouldn't match
+        String phone2 = "(123) 456-7890"; // match
+        String phone3 = "123 456-7890"; // Shouldn't match
+        String phone4 = "(123)456-7890"; // Shouldn't match
+
+        System.out.println("Phone1 : " + phone1 + " : " + phone1.matches(usPhoneNumberRegex));
+        System.out.println("Phone2 : " + phone2 + " : " + phone2.matches(usPhoneNumberRegex));
+        System.out.println("Phone3 : " + phone3 + " : " + phone3.matches(usPhoneNumberRegex));
+        System.out.println("Phone4 : " + phone4 + " : " + phone4.matches(usPhoneNumberRegex));
+
+
+
+        System.out.println("\n-------------- VISA CARD VALIDATION -------------");
+        // ^4[0-9]{12}([0-9]{3})?$
+        String visaCardRegex = "^4[0-9]{12}([0-9]{3})?$";
+
+        String visa1 = "4444444444444"; // should match
+        String visa2 = "5444444444444"; // shouldn't match
+        String visa3 = "4444444444444444";  // should match
+        String visa4 = "4444";  // shouldn't match
+
+        System.out.println("visa1 : " + visa1 + " : " + visa1.matches(visaCardRegex));
+        System.out.println("visa2 : " + visa2 + " : " + visa2.matches(visaCardRegex));
+        System.out.println("visa3 : " + visa3 + " : " + visa3.matches(visaCardRegex));
+        System.out.println("visa4 : " + visa4 + " : " + visa4.matches(visaCardRegex));
 
     }
 }
