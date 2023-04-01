be578215302c91142ea091438bbba85887d17c84
Gauri Patil <gauri.be0811@gmail.com>
2023-04-01 23:46:06 +0530

    JUnit - Bank Account App - Basic JUnit test - part 2

---
+++ JUnit/JUnitBankAccountDemoApp/src/BankAccountTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        fail("This test is yet to implement.");
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+        fail("This test is yet to implement.");
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("This test is yet to implement.");
+    }
+}
