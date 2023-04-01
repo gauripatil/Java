a0c0ceae5f07f432ac1d2b1016bf4ad64a238951
Gauri Patil <gauri.be0811@gmail.com>
2023-04-02 00:08:32 +0530

    JUnit - Bank Account App - Added test for - isChecking part 2

--- JUnit/JUnitBankAccountDemoApp/src/BankAccountTest.java
+++ JUnit/JUnitBankAccountDemoApp/src/BankAccountTest.java
@@ -4,7 +4,7 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00,  BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
     }
@@ -15,9 +15,23 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void getBalance() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+    public void getBalance_deposit() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
+
+    @org.junit.Test
+    public void getBalance_withdraw() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        assertEquals(true, account.isChecking());
+    }
+
 }
