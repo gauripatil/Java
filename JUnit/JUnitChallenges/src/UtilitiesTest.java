9d0147af7bd274b75f799e7ac3262e6d2d1cd173
Gauri Patil <gauri.be0811@gmail.com>
2023-04-04 23:56:03 +0530

    JUnit - Utilities App - Challenges - part 1

---
+++ JUnit/JUnitChallenges/src/UtilitiesTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @Test
+    public void everyNthChar() {
+    }
+
+    @Test
+    public void removePairs() {
+    }
+
+    @Test
+    public void converter() {
+    }
+
+    @Test
+    public void nullIfOddLength() {
+    }
+}
