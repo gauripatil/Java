26d2f2e8b869e44c31dd87ec247bd5917bb491d8
Gauri Patil <gauri.be0811@gmail.com>
2023-04-28 22:08:55 +0530

    SQLite DB - Create connection & table

--- SqliteDbApp/src/Main.java
+++ SqliteDbApp/src/Main.java
@@ -1,11 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            // This line of code added to resolve - java.sql.SQLException: No suitable driver found for jdbc:sqlite
+            Class.forName("org.sqlite.JDBC");
+
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/gauri/Documents/Java/SqliteDbApp/testjava.db");
+            Statement statement = connection.createStatement();
+
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, EMAIL TEXT)");
+            statement.close();
+            connection.close();
+
+
             System.out.println("Try to connect to db");
-        } catch (Exception e) {
-            System.out.println("Something went wrong");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Something went wrong" + e.toString());
         }
     }
 }
