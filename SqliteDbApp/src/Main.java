8481271a9b663c39279f70269dbc9a6f535f7383
Gauri Patil <gauri.be0811@gmail.com>
2023-04-29 21:32:41 +0530

    Added insert, uodate delete queries - Contacts app

--- SqliteDbApp/src/Main.java
+++ SqliteDbApp/src/Main.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,10 +9,76 @@ public class Main {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/gauri/Documents/Java/SqliteDbApp/testjava.db");
             Statement statement = connection.createStatement();
 
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, EMAIL TEXT)");
+            statement.execute("CREATE TABLE IF NOT EXISTS " +
+                                    "contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                    "VALUES('Gauri', 465774, 'gauri@email.com')");
+
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                              "VALUES('Joe', 45632, 'joe@anywhere.com')");
+
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                    "VALUES('Jane', 4829484, 'jane@somewhere.com')");
+
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                    "VALUES('Fido', 9038, 'dog@email.com')");
+
+            statement.execute("UPDATE contacts SET phone=554433 WHERE name='Gauri'");
+            statement.execute("DELETE from contacts WHERE name='Fido'");
+
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet result = statement.getResultSet();
+
+            while (result.next()) {
+                System.out.println(result.getString("name") + " " +
+                        result.getString("phone") + " " +
+                        result.getString("email"));
+            }
+            result.close();
             statement.close();
             connection.close();
 
+            /**
+             * Now every result set has a cursor.
+             *
+             * Now this cursor isn't the same as a database cursor.
+             *
+             * We could have had several result set objects and each one will have a cursor.
+             *
+             * However, and this is actually quite important,
+             *
+             * if we re-use a statement object to do a query,
+             *
+             * then any result set associated with that statement object is closed
+             *
+             * and a new one is created for the new query.
+             *
+             * So if we want to work with several query results at the same time
+             *
+             * then it's imperative to use a different statement instance for each query.
+             *
+             * And keep in mind that a statement object ultimately can only ever have one
+             *
+             * active result set associated with it.
+             *
+             * Now note that result set is a resource.
+             *
+             * So we have to close it. And you can see that I added
+             *
+             * the results dot close on line 35.
+             *
+             * And that's because it's associated with the statement instance.
+             *
+             * We have to close it before we close the statement.
+             *
+             * Now we're closing all our resources at the end of the try block.
+             *
+             * If an exception is thrown,
+             *
+             * it's possible that the closed methods won't be executed.
+             */
 
             System.out.println("Try to connect to db");
         } catch (SQLException | ClassNotFoundException e) {
